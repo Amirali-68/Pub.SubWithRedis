66b0bc4be4367cfd7d3d60967f641480de59974b
amirali <a.rouki@dgland.com>
2022-07-08 14:50:54 +0430

    init

---
+++ Publisher/Program.cs
@@ -0,0 +1,46 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace Publisher
+{
+    internal class Program
+    {
+        private const string RedisConnectionString = "127.0.0.1:6379";
+        private static ConnectionMultiplexer connection =
+                        ConnectionMultiplexer.Connect(RedisConnectionString);
+        private const string Channel = "message-channel";
+        private static string message = string.Empty;
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Enter your message: ");
+                message = Console.ReadLine();
+
+                var pubsub = connection.GetSubscriber();
+
+                pubsub.PublishAsync(Channel, message);
+                MessageAction(message);
+
+
+            }
+        }
+        static void MessageAction(string message)
+        {
+            int initialCursorTop = Console.CursorTop;
+            int initialCursorLeft = Console.CursorLeft;
+
+#pragma warning disable CA1416 // Validate platform compatibility
+            Console.MoveBufferArea(0, initialCursorTop, Console.WindowWidth, 1, 0, initialCursorTop + 1);
+#pragma warning restore CA1416 // Validate platform compatibility
+            Console.CursorTop = initialCursorTop;
+            Console.CursorLeft = 0;
+
+            // Print the message here
+            Console.WriteLine($"'{message}' Successfully sent to message-channel");
+
+            Console.CursorTop = initialCursorTop + 1;
+            Console.CursorLeft = initialCursorLeft;
+        }
+    }
+}
---
+++ Subscriber/Program.cs
@@ -0,0 +1,23 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace Subscriber
+{
+    internal class Program
+    {
+        private const string RedisConnectionString = "127.0.0.1:6379";
+        private static ConnectionMultiplexer connection =
+                        ConnectionMultiplexer.Connect(RedisConnectionString);
+        private const string Channel = "message-channel";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Listening message-channel");
+            var pubsub = connection.GetSubscriber();
+
+            pubsub.Subscribe(Channel, (channel, message) => Console.Write
+                        ("Message received from message-channel : " + message));
+
+            Console.ReadLine();
+        }
+    }
+}
